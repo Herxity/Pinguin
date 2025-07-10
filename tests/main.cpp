576deee428a2944ccf4cbc8a5ec18053b47c5020
Herxity <ruben.alias715@gmail.comm>
2025-07-09 21:49:44 -0700

    Fixed some gitignore stuff

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,12 @@
 #include "../include/server.h"
 #include <unistd.h>
 #include <sys/wait.h>
-#include <curl\curl.h>
+#include <curl/curl.h>
+
+size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
+    data->append((char*) ptr, size * nmemb);
+    return size * nmemb;
+}
 
 int main(){
     pid_t pid = fork();
@@ -11,19 +16,22 @@ int main(){
         exit(EXIT_FAILURE);
     }else if (pid > 0) {
         std::cout << "printed from parent process " << getpid() << std::endl;
-        std::CURL *curl = curl_easy_init();
+        CURL *curl = curl_easy_init();
         if(curl) {
-            const char *data = "submit = 1";
 
-            curl_easy_setopt(curl, CURLOPT_URL, "http://10.5.10.200/website/WebFrontend/backend/posttest.php");
+            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/");
 
             /* size of the POST data */
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, 10L);
-
+            curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+            std::string response_string;
+            std::string header_string;
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
             /* pass in a pointer to the data - libcurl will not copy */
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
             curl_easy_perform(curl);
+            std::cout << response_string << std::endl;
         }
         waitpid(pid, &status, 0);
         if (WIFSIGNALED(status)){
@@ -40,7 +48,7 @@ int main(){
             res.setProtocol(req.getHttpProtocol());
             res.setHeaders({ {"content-type","text/html"}});
             res.setCode("200","OK");
-            res.sendFile("index.html");
+            res.sendFile("../success.txt");
         });
         app.listen();
     }
